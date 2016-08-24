93061da373eb3e9836b62f2df2b568b8699ff455
sathish.n <sathish.n@sathish.n>
2016-08-24 20:50:33 +0530

    Added greenDao

--- MastApp/app/src/main/java/com/mast/android/orm/views/MainActivity.java
+++ MastApp/app/src/main/java/com/mast/android/orm/views/MainActivity.java
@@ -17,6 +17,12 @@ import com.mast.orm.db.MastOrm;
 
 import java.util.List;
 
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 insert1000Rows();
             }
         });
+        initiateGreenDao();
 //        Debug.stopMethodTracing();
     }
 
@@ -56,6 +63,47 @@ public class MainActivity extends AppCompatActivity {
 
         Debug.stopMethodTracing();
         Toast.makeText(this,"10 rows inserted", Toast.LENGTH_LONG).show();
+    }
+
+    private void initiateGreenDao(){
+        final String PROJECT_DIR = System.getProperty("user.dir");
+
+            Schema schema = new Schema(1, "com.abc.greendaoexample.db");
+            schema.enableKeepSectionsByDefault();
+
+            addTables(schema);
+
+            try {
+                new DaoGenerator().generateAll(schema, PROJECT_DIR + "\\app\\src\\main\\java");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    private void addTables(final Schema schema) {
+        Entity user = addUser(schema);
+        Entity repo = addRepo(schema);
+
+        Property userId = repo.addLongProperty("userId").notNull().getProperty();
+        user.addToOne(repo, userId, "userRepos");
+    }
+
+    private Entity addUser(final Schema schema) {
+        Entity user = schema.addEntity("User");
+        user.addIdProperty().primaryKey().autoincrement();
+        user.addStringProperty("name").notNull();
+        user.addShortProperty("age");
+        return user;
+    }
+
+    private Entity addRepo(final Schema schema) {
+        Entity repo = schema.addEntity("Repo");
+        repo.addIdProperty().primaryKey().autoincrement();
+        repo.addStringProperty("title").notNull();
+        repo.addStringProperty("language");
+        repo.addIntProperty("watchers_count");
+
+        return repo;
     }
 
 
